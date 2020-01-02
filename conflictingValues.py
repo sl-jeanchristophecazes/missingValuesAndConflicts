db0b3c5fcc8d55e763d1b01e5532b491644a9ad4
Jean-Christophe Cazes <jean-christophe.cazes@groupeseloger.com>
2020-01-02 15:06:16 +0100

    correctedConflictingValues

--- conflictingValues.py
+++ conflictingValues.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 
 def conflictingDataTypes(df):
-    """Takes a pandas dataframe, and returns a dictionnary containing the conflicting values of 
+    """Takes a pandas dataframe, and returns two dictionnaries. The first one contains the different types of 
     each column. 
-    key: column with conflcit 
+    key: column name 
     value: list of the different types a column has.
+
+    The second one contains the conflicts:
+    key: column with conflicting types
+    value: the list of different types within the column
     
     Arguments:
         df {pandas} -- The dataframe to inspect
@@ -25,11 +29,16 @@ def conflictingDataTypes(df):
     for k,v in valueTypeDict.items():
         print(k,' has the following types of values: ',v)
 
+
+    conflictsDict = {}
+
     for k,v in valueTypeDict.items():
         if len(v) != 1:
             print(k,' has conflicting data types')
             for types in v:
                 print(types)
 
-    return(valueTypeDict)
+            conflictsDict[k] = v
+
+    return(valueTypeDict,conflictsDict)
     
